69a24a620be9f7b0ae31ef49528acceda6377325
roman <roman@ridgebox.com>
2018-09-30 00:06:55 +0300

    Built distributive
    Built distributive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'styles/base.scss';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
